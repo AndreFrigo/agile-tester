89dfa67910607538dfd1c2f6c459cd5ab362f9dc
Andrea Frigo <andrea.boy.frigo@gmail.com>
2020-03-23 19:28:31 +0100

    Create project

---
+++ test/test.js
@@ -0,0 +1,54 @@
+var Application = require('spectron').Application
+var assert = require('assert');
+
+var app = null;
+
+//apre l'applicazione prima di iniziare il test
+before(function(done) {
+    console.log('before every test in every file');
+    app = new Application({
+        path: 'C:\\Program Files (x86)\\Praim\\Agile\\AgileConfigurator\\AgileConfigurator.exe'
+    });
+    console.log("Applicazione caricata");
+    app.start();
+    done();
+});
+
+//chiude l'applicazione alla fine dei test
+after(function (done) {
+    if (app && app.isRunning()) {
+        app.stop();
+    }
+    done();
+});
+
+
+
+describe('Test', function(){
+
+    //pausetta tra i vari test
+    beforeEach(function(done){
+        this.timeout(5000);
+        setTimeout(done,4000);
+    })
+
+    //Controlla se l'applicazione è attiva
+    it('App is running', async () => {
+        var running = app.isRunning();
+        assert.equal(running, true);
+    });
+
+    //Controlla che sia aperta una singola finestra dell'applicazione
+    it('Shows an initial window', async () => {
+        app.client.waitUntilWindowLoaded();
+        const count = await app.client.getWindowCount();
+        assert.equal(count, 1);
+      });
+
+})
+
+
+
+
+
+
