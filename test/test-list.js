6546e8daacaa5c6083833de3a2657a3242d76364
Andrea Frigo <andrea.boy.frigo@gmail.com>
2020-03-24 09:56:25 +0100

    Add file test-list

---
+++ test/test-list.js
@@ -0,0 +1,11 @@
+//contiene la lista dei test e permette di attivarli/disattivarli
+
+const testList={
+    //Controlla se l'applicazione è attiva
+    appIsRunning: false,
+    
+    //Controlla che sia aperta una singola finestra dell'applicazione
+    onlyOneWindow: true
+};
+
+module.exports = {testList};
