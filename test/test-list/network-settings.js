ac231686f1913ce859fe2a3a9b30090acd012674
Andrea Frigo <andrea.boy.frigo@gmail.com>
2020-06-22 17:18:27 +0200

    Add new error handling to network-settings.js

--- test/test-list/network-settings.js
+++ test/test-list/network-settings.js
@@ -8,28 +8,52 @@ function addWifiNetwork(ssid, psw){
 
         //Va nella sezione impostazioni di rete
         it("Navigates to Network Settings", async () => {
-            const click = await global.app.client.click('#menu-link-2');
+            const menu = global.app.client.$('#menu-link-2');
+            var click = null;
+            try{
+                click = await menu.click();
+            }catch{
+                assert.ok(false, "Impossible to find the network settings link")
+            }
             global.app.client.waitUntilWindowLoaded();
             assert.ok(click, "error while opening the network settings menù");
         })
 
         //Va nella sezione WiFi
         it("Navigates to WiFi", async () => {
-            const click = await global.app.client.$("#ab > a").click();
+            const menu = global.app.client.$("#ab > a");
+            var click = null;
+            try{
+                click = await menu.click();
+            }catch{
+                assert.ok(false, "Impossible to find the WiFi tab")
+            }
             global.app.client.waitUntilWindowLoaded();
             assert.ok(click, "error while opening the WiFi tab");
         })
 
         //Clicca su aggiungi rete wifi
         it("Click on Add WiFi Network", async () => {
-            const click = await global.app.client.$("#wifiTab > div > div > div.header-inputs > a").click();
+            const button = global.app.client.$("#wifiTab > div > div > div.header-inputs > a");
+            var click = null;
+            try{
+                click = await button.click();
+            }catch{
+                assert.ok(false, "Impossible to find the Add WiFi button")
+            }
             global.app.client.waitUntilWindowLoaded();
             assert.ok(click, "Error while clicking the Add WiFi Network link");
         })
 
         //Apri la lista delle wifi disponibili
         it("Opens available WiFi list", async () => {
-            const click = await global.app.client.$("#wifi").click();
+            const button = global.app.client.$("#wifi");
+            var click = null;
+            try{
+                click = await button.click();
+            }catch{
+                assert.ok(false, "Impossible to find the WiFi list")
+            }
             global.app.client.waitUntilWindowLoaded();
             assert.ok(click, "Error while opening available WiFi list");
         })
@@ -47,8 +71,15 @@ function addWifiNetwork(ssid, psw){
         //Inserisce la password
         it("Insert password", async () => {
             const password = global.app.client.$("#add-connection-modal > div > div.modal-content > div > div > div.row:nth-child(4) > div > div > input");
-            password.click();
-            password.setValue(psw);
+            try{
+                password.click();
+                var x = await password.setValue(psw);
+                while(!x){
+                    
+                }
+            }catch{
+                assert.ok(false, "Impossible to find password field")
+            }
             password.getValue().then(function(v){
                 assert.equal(v, psw, "error while inserting the password");
             })
@@ -56,7 +87,13 @@ function addWifiNetwork(ssid, psw){
 
         //Preme ok per confermare 
         it("Add the new WiFi clicking ok", async () => {
-            const click = await global.app.client.$("#add-connection-modal > div > div.modal-footer > div > a:nth-child(1)").click();
+            const ok = global.app.client.$("#add-connection-modal > div > div.modal-footer > div > a:nth-child(1)");
+            var click = null;
+            try{
+                click = await ok.click();
+            }catch{
+                assert.ok(false, "Impossible to find ok button")
+            }
             assert.ok(click, "Error while clicking ok")
         })
 
@@ -71,8 +108,13 @@ function addWifiNetwork(ssid, psw){
             const length = await db.getWifiListLength();
             const base = "#wifiTab > div > div > div.section-wrapper.scrollable > div"
             var found = false; 
+            var name = null;
             for(i = 1; i<= length; i++){
-                const name = await global.app.client.$(base + " > div:nth-child(" + i + ") > div > div.block-item-properties-wrapper > div").getText();
+                try{
+                    name = await global.app.client.$(base + " > div:nth-child(" + i + ") > div > div.block-item-properties-wrapper > div").getText();
+                }catch{
+                    assert.ok(false, "Impossible to find the list elements")
+                }
                 if(name.slice(14) == ssid){
                     found = true;
                 }
