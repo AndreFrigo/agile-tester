33f38d749b9827457426b877234e44a37d476d8e
Andrea Frigo <andrea.boy.frigo@gmail.com>
2020-06-22 17:41:23 +0200

    Add new error handling to resources.js

--- test/test-list/resources.js
+++ test/test-list/resources.js
@@ -13,28 +13,54 @@ function addResource(resourceName, resourceUrl){
 
         //va nelle risorse
         it("Navigates to resources", async () => {
-            const click = await global.app.client.click('#menu-link-6');
+            const menu = global.app.client.$("#menu-link-6");
+            var click = null;
+            try{
+                click = await menu.click();
+            }catch{
+                assert.ok(false, "Impossible to find the resources menù")
+            }
             global.app.client.waitUntilWindowLoaded();
             assert.ok(click, "error while opening the resources menù");
         })
 
         //clicca su add resource
         it("Clicks on Add Resource", async () => {
-            const click = await global.app.client.$("#main-div > div.main-content > main > section > div > div.fixed-header > div > a").click();
+            const button = global.app.client.$("#main-div > div.main-content > main > section > div > div.fixed-header > div > a");
+            var click = null;
+            try{
+                click = await button.click();
+            }catch{
+                assert.ok(false, "Impossible to find the add resource button")
+            }
             global.app.client.waitUntilWindowLoaded();
             assert.ok(click, "error while clicking Add Resource");
         })
 
         //setta local browser come resource type
         it("Select local browser as Resource Type", async () => {
-            const click = await global.app.client.$("#add-connection-modal > div.custom-modal.open > div.modal-content > div > div > div.row > div.col.s12 > div.connection-col > div:nth-child(4) > label").click();
-            assert.ok(click, "error while selecting local application");
+            const button = global.app.client.$("#add-connection-modal > div.custom-modal.open > div.modal-content > div > div > div.row > div.col.s12 > div.connection-col > div:nth-child(4) > label");
+            var click = null;
+            try{
+                click = await button.click();
+            }catch{
+                assert.ok(false, "Impossible to find the local browser checkbox")
+            }
+            assert.ok(click, "error while selecting local browser");
         })
 
         //inserisce il nome della risorsa da creare 
         it("Insert resource name", async () => {
             const text = global.app.client.$("#add-connection-name");
-            text.setValue(resourceName);
+            try{
+                text.click();
+                var x = await text.setValue(resourceName);
+                while(!x){
+                    
+                }
+            }catch{
+                assert.ok(false, "Impossible to find name field")
+            }
             text.getValue().then(function(v){
                 assert.equal(resourceName, v, "error while inserting the resource name");
             })
@@ -43,15 +69,29 @@ function addResource(resourceName, resourceUrl){
         //inserisce l'url della pagina da visitare 
         it("Insert URL", async () => {
             const url = global.app.client.$("#add-connection-server");
-            url.setValue(resourceUrl);
+            try{
+                url.click();
+                var x = await url.setValue(resourceUrl);
+                while(!x){
+                    
+                }
+            }catch{
+                assert.ok(false, "Impossible to find url field")
+            }
             url.getValue().then(function(v){
-                assert.equal(resourceUrl, v, "error while inserting the resource name");
+                assert.equal(resourceUrl, v, "error while inserting the resource url");
             })
         })
 
         //conferma
         it("Click on Ok to confirm", async () => {
-            const click = await global.app.client.$(".a=Ok").click();
+            const ok = global.app.client.$(".a=Ok");
+            var click = null;
+            try{
+                click = await ok.click();
+            }catch{
+                assert.ok(false, "Impossible to find ok button")
+            }
             assert.ok(click, "Error while clicking ok to confirm")
         })
 
@@ -75,10 +115,16 @@ function addResource(resourceName, resourceUrl){
         it("Check if the resource is in the agile list", async () => {
             const length = await db.getResourceListLength();
             var found = false;
+            var n = null;
+            var u = null;
             for(i = 0; i < length; i++){
                 const base = "#connection"+i+" > div > div.connection-item-properties > div";
-                const n = await global.app.client.$(base + " > div").getText();
-                const u = await global.app.client.$(base + " > p > span").getText(); 
+                try{
+                    n = await global.app.client.$(base + " > div").getText();
+                    u = await global.app.client.$(base + " > p > span").getText();
+                }catch{
+                    assert.ok(false, "Impossible to find the list elements")
+                } 
                 if(n == "agile_local "+ resourceName && u == "subdirectory_arrow_right" + resourceUrl){
                     found = true;
                 }
