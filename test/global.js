a5fcd05cfae521c46a0a0e2860c6670a99f3cf99
Andrea Frigo <andrea.boy.frigo@gmail.com>
2020-06-23 17:21:32 +0200

    Add async function to sleep

--- test/global.js
+++ test/global.js
@@ -3,6 +3,8 @@ const global={
     app: null,
 
     // Italiano:1, Inglese:2, Spagnolo:3
-    language: null
+    language: null,
+
+    sleep : time => new Promise(r => setTimeout(r, time))
 }
 module.exports = {global};
