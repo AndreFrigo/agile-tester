63d783a676de2917bd6d5cf9475fd597676c6793
Andrea Frigo <andrea.boy.frigo@gmail.com>
2020-06-23 09:37:39 +0200

    Add new error handling to general.js

--- test/test-list/general.js
+++ test/test-list/general.js
@@ -35,13 +35,17 @@ function checkLanguage(){
             global.language = await db.dbLanguage();
 
             //controllo lingua di agile dall'interfaccia
-            const lang = global.app.client.$('#menu-link-1');
-            await lang.getText().then(function(l){
-                if(l == "Impostazioni di Sistema") assert.equal(global.language, 1)
-                else if(l == "System Settings") assert.equal(global.language, 2)
-                else if(l == "Ajustes del Sistema") assert.equal(global.language, 3)
-                else assert.ok(false, "error while checking the language")
-            })
+            var l = null;
+            try{
+                const lang = global.app.client.$('#menu-link-1');
+                l = await lang.getText();
+            }catch{
+                assert.ok(false, "Impossible to find the text for checking language")
+            }
+            if(l == "Impostazioni di Sistema") assert.equal(global.language, 1)
+            else if(l == "System Settings") assert.equal(global.language, 2)
+            else if(l == "Ajustes del Sistema") assert.equal(global.language, 3)
+            else assert.ok(false, "error while checking the language")
         });
     })
 }
