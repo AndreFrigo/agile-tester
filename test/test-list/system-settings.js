73f07fb9c773ca69f7aa6e69d8360491e1ed6702
Andrea Frigo <andrea.boy.frigo@gmail.com>
2020-06-24 13:00:57 +0200

    change way to test in system-settings.js, now each describe has to open and close the app

--- test/test-list/system-settings.js
+++ test/test-list/system-settings.js
@@ -1,115 +1,204 @@
 const {db} = require ("../db.js");
 const {global} = require ("../global.js");
-var assert = require('assert');
+const { expect } = require("chai");
+
+//input lan indica la lingua corrente 
+const chooseEnglish = async function(lan){
+
+    //va nelle impostazioni di sistema
+    const menu = global.app.client.$('#menu-link-1');
+    var click = null;
+    try{
+        click = await menu.click();
+    }catch{
+    }
+    global.app.client.waitUntilWindowLoaded();
+
+
+    await global.sleep(1000)
+
+
+    //va nella sezione lingua
+    const language = global.app.client.$("#language-tab.tab > a");
+    click = null;
+    try{
+        click = await language.click();
+    }catch{
+    }
+    global.app.client.waitUntilWindowLoaded();
+
+
+    await global.sleep(1000)
+
+
+    //apre la scelta della lingua
+    const button = global.app.client.$("#language > span > div > div > div > input.select-dropdown");
+    click = null;
+    try{
+        click = await button.click();
+    }catch{
+    }
+
+
+    await global.sleep(1000)
+
+
+    //setta la lingua inglese 
+    var ret = null
+    if(lan == 1){
+        try{
+            ret = await global.app.client.click("span=Inglese")
+            //aggiorna la lingua corrente
+            global.language = 2;
+        }catch{
+            ret = null
+        }
+    }else if(lan == 2){
+        try{
+            ret = await global.app.client.click("span=English")
+            //aggiorna la lingua corrente
+            global.language = 2;
+        }catch{
+            ret = null
+        }
+    }else if (lan == 3){
+        try{
+            ret = await global.app.client.click("span=Inglés")
+            //aggiorna la lingua corrente
+            global.language = 2;
+        }catch{
+            ret = null
+        }
+    }
+    return ret
+}
+
 
 function setEnglishLanguage(){
     //Imposta la lingua di agile in english
     describe("Choose english as language", function(){
 
-        //Va nella sezione impostazioni
-        it('Navigates to settings', async () => {
-            const menu = global.app.client.$('#menu-link-1');
-            var click = null;
-            try{
-                click = await menu.click();
-            }catch{
-                assert.ok(false, "Impossible to find the system settings link")
-            }
-            global.app.client.waitUntilWindowLoaded();
-            assert.ok(click, "error while opening the settings menù");
-        });
-        
-        
-        //Dalla sezione impostazioni va a quella della lingua
-        it('Navigates to language', async () => {
-            const menu = global.app.client.$("#language-tab.tab > a");
-            var click = null;
-            try{
-                click = await menu.click();
-            }catch{
-                assert.ok(false, "Impossible to find the WiFi tab")
-            }
-            global.app.client.waitUntilWindowLoaded();
-            assert.ok(click, "error while opening the language tab");
-        });
+        this.timeout(30000)
 
-        //Controllo che la lingua selezionata sia quella attuale 
-        it('Control that displayed current language is correct', async () => {
-            const lan = global.app.client.$('#language > span > div > div > div > input.select-dropdown');
-            try{
-                var l = await lan.getValue();
-            }catch{
-                assert.ok(false, "Impossible to find the lable language")
-            }            
-            if(l == "Italiano") assert.equal(global.language, 1)
-            else if(l == "English") assert.equal(global.language, 2)
-            else if(l == "Español") assert.equal(global.language, 3)
-            else assert.ok(false, "error while checking language")
+        describe("Test from spanish", async () => {
+
+            before(async function(){    
+                //Setta italiano
+                db.conn.select(1)
+                db.conn.set("config_locale", "{\"current_keyboard_locale\":\"it-IT\",\"current_locale_agile\":\"es-ES\"}")
+                await global.app.start()
+            }) 
+
+            after(async function(){
+                await global.app.stop()
+            })
+
+            it("should return true if the language changed to english", async () => {
+                await chooseEnglish(3)
+                var ret = false
+                await global.sleep(1500)
+                const lan = await db.dbLanguage()
+                ret = (lan == 2)
+                expect(ret).to.be.true
+            })
+
+            it("should return true if agile and database language are the same", async () => {
+                //controllo lingua di agile dall'interfaccia
+                var l = null;
+                try{
+                    const lang = global.app.client.$('#menu-link-1');
+                    l = await lang.getText();
+                }catch{
+                }
+                var ret = false
+                if(l == "Impostazioni di Sistema") ret = (global.language == 1)
+                else if(l == "System Settings") ret = (global.language == 2)
+                else if(l == "Ajustes del Sistema") ret = (global.language == 3)
+                expect(ret).to.be.true
+            })
+
         })
 
-        //Dalla sezione lingua, apre la scelta della lingua
-        it('Open language list', async () => {
-            const button = global.app.client.$("#language > span > div > div > div > input.select-dropdown");
-            var click = null;
-            try{
-                click = await button.click();
-            }catch{
-                assert.ok(false, "Impossible to find the button")
-            }
-            assert.ok(click, "error while clicking the button");
-        });
+        
+        describe("Test from italian", async () => {
 
-        //Sceglie la lingua inglese
-        it('Select english as language', async () => {
-            var choice = ""
-            switch(global.language){
-                case 1 : {
-                    choice+="span=Inglese";
-                    break;
-                }
-                case 2 : {
-                    choice+="span=English";
-                    break;
-                }
-                case 3 : {
-                    choice+="span=Inglés";
-                    break;
-                }
-                default : {
-                    console.log("Undefined language! " + global.language);
-                    break;
-                }
-            }
-            try{
-                await global.app.client.click(choice).then(function(){
-                    //aggiorna la lingua corrente
-                    global.language = 2;
-                });
-            }catch{
-                assert.ok(false, "Impossible to select the language")
-            }
-            //controlla che la lingua corrente sia inglese
-            assert.equal(global.language,2, "error, the current language is not english");
-        });
+            before(async function(){    
+                //Setta italiano
+                db.conn.select(1)
+                db.conn.set("config_locale", "{\"current_keyboard_locale\":\"it-IT\",\"current_locale_agile\":\"it-IT\"}")
+                await global.app.start()
+            }) 
 
-        it('Check if db language is english', async () => {
-            var lan = await db.dbLanguage();
-            assert(lan, "en-GB", "error, the db language is not english");
-        });
+            after(async function(){
+                await global.app.stop()
+            })
 
-        //Controllo che la lingua selezionata sia english
-        it('Control that displayed current language is english', async () => {
-            const lan = global.app.client.$('#language > span > div > div > div > input.select-dropdown');
-            try{
-                var l = await lan.getValue();
-            }catch{
-                assert.ok(false, "Impossible to find the lable language")
-            } 
-            if(l == "Italiano") assert.equal(global.language, 1)
-            else if(l == "English") assert.equal(global.language, 2)
-            else if(l == "Español") assert.equal(global.language, 3)
-            else assert.ok(false, "error while checking language")
+            it("should return true if the language changed to english", async () => {
+                await chooseEnglish(1)
+                var ret = false
+                await global.sleep(1500)
+                const lan = await db.dbLanguage()
+                ret = (lan == 2)
+                expect(ret).to.be.true
+            })
+
+            it("should return true if agile and database language are the same", async () => {
+                //controllo lingua di agile dall'interfaccia
+                var l = null;
+                try{
+                    const lang = global.app.client.$('#menu-link-1');
+                    l = await lang.getText();
+                }catch{
+                }
+                var ret = false
+                if(l == "Impostazioni di Sistema") ret = (global.language == 1)
+                else if(l == "System Settings") ret = (global.language == 2)
+                else if(l == "Ajustes del Sistema") ret = (global.language == 3)
+                expect(ret).to.be.true
+            })
+
         })
+
+
+        describe("Test from english", async () => {
+
+            before(async function(){    
+                //Setta italiano
+                db.conn.select(1)
+                db.conn.set("config_locale", "{\"current_keyboard_locale\":\"it-IT\",\"current_locale_agile\":\"en-GB\"}")
+                await global.app.start()
+            }) 
+
+            after(async function(){
+                await global.app.stop()
+            })
+
+            it("should return true if the language changed to english", async () => {
+                await chooseEnglish(2)
+                var ret = false
+                await global.sleep(1500)
+                const lan = await db.dbLanguage()
+                ret = (lan == 2)
+                expect(ret).to.be.true
+            })
+
+            it("should return true if agile and database language are the same", async () => {
+                //controllo lingua di agile dall'interfaccia
+                var l = null;
+                try{
+                    const lang = global.app.client.$('#menu-link-1');
+                    l = await lang.getText();
+                }catch{
+                }
+                var ret = false
+                if(l == "Impostazioni di Sistema") ret = (global.language == 1)
+                else if(l == "System Settings") ret = (global.language == 2)
+                else if(l == "Ajustes del Sistema") ret = (global.language == 3)
+                expect(ret).to.be.true
+            })
+
+        })
+
     })
 }
 
