a58875355e054df20a9a66d24701e42504f48ef3
Andrea Frigo <andrea.boy.frigo@gmail.com>
2020-06-29 15:48:38 +0200

    add other tests to remote assistance

--- test/remote-assistance/check-remote-assistance.js
+++ test/remote-assistance/check-remote-assistance.js
@@ -37,5 +37,72 @@ describe("check remote assistance settings", function(){
         expect(raAgile && radb.enabled).to.be.true
     })
 
+    it("should return true if show notification icon is checked", async () => {
+        await utils.enableRemoteAssistance()
+        await utils.sleep(500)
+        const checkbox = global.app.client.$("#show-icon")
+        var val = null
+        var isEnable = null
+        var click = null
+        try{
+            val = await checkbox.getValue()
+        }catch{
+        }
+        if(val == "false"){
+            const label = global.app.client.$("#main-div > div.main-content > main > section > div > div.row:nth-child(2) > div > div.row > div > div > label")
+            try{
+                click = label.click()
+            }catch{
+            }
+        }else if (val == "true"){
+            isEnable = true
+        }
+        if(click != null) isEnable = true
+        await utils.sleep(500)
+        const radb = await db.getRemoteAssistance()
+        expect(isEnable && radb.appearance.show_icon).to.be.true
+    })
+
+    it("should return true if require user authentication is checked", async () => {
+        await utils.enableRemoteAssistance()
+        await utils.sleep(500)
+        const checkbox = global.app.client.$("#allow-reject")
+        var val = null
+        var isEnable = null
+        var click = null
+        try{
+            val = await checkbox.getValue()
+        }catch{
+        }
+        if(val == "false"){
+            const label = global.app.client.$("#main-div > div.main-content > main > section > div > div.row:nth-child(2) > div > div.row:nth-child(2) > div > div > label")
+            try{
+                click = label.click()
+            }catch{
+            }
+        }else if (val == "true"){
+            isEnable = true
+        }
+        if(click != null) isEnable = true
+        await utils.sleep(500)
+        const radb = await db.getRemoteAssistance()
+        expect(isEnable && radb.acceptance.allow_reject).to.be.true
+    })
+
+    const rightValues = [23, 3e2]
+    rightValues.forEach(elem => {
+        it("should return true if auto-accept and enable remote assistance is checked and his lable is correct", async () => {
+            expect(await utils.setAutoAccept(elem)).to.be.true
+        })
+    })
+
+    const wrongValues = [0, "abc", 1234567, "", -7, 2e7]
+    wrongValues.forEach(elem => {
+        it("should return false if auto-accept or enable remote assistance are not checked, or if the lable is not correct", async () => {
+            expect(await utils.setAutoAccept(elem)).to.be.false
+        })
+    })
+    
+
 
 })
