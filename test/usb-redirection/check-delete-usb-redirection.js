9b8e72783bf7e1fab67d21eb13e69e10419afd13
Andrea Frigo <andrea.boy.frigo@gmail.com>
2020-07-01 10:36:25 +0200

    add usb-redirection/check-delete-usb-redirection.js

---
+++ test/usb-redirection/check-delete-usb-redirection.js
@@ -0,0 +1,64 @@
+const {db} = require ("../db.js");
+const {global} = require ("../global.js");
+const {utils} = require("../utils.js");
+const { expect } = require("chai");
+var localDB = null
+
+describe("delete usb rule tests", function(){
+
+    this.timeout(30000)
+
+    before(async function(){
+        //salva database locale
+        db.conn.select(1)
+        localDB = await new Promise(function (resolve, reject){
+            db.conn.get("config_citrix", function(err, res){
+                if(err) reject(err)
+                resolve(res)
+            });
+        })
+    })
+
+    beforeEach(async function(){
+        await global.app.start()
+        //cambia database locale
+        db.conn.select(1)
+        db.conn.set("config_citrix", "{\"usb_redirection\":{\"auto_redirect_on_start\":true,\"auto_redirect_on_plug\":false,\"rules\":[{\"type\":\"ALLOW\",\"vid\":\"1234\",\"pid\":\"1234\",\"class\":\"\",\"subclass\":\"\",\"prot\":\"\",\"description\":\"prova_usb\",\"split\":false,\"interfaces\":[]}],\"default\":\"ALLOW\"}}")
+    }) 
+
+    afterEach(async function(){
+        db.conn.select(1)
+        db.conn.set("config_citrix", localDB)
+        await global.app.stop()
+    })
+
+
+    it("should return true if the rule has been delete successfully", async () => {
+        expect(await utils.usbRedirection.deleteRule(1234, 1234)).to.be.true
+    })
+
+    it("should return false if it tries to delete a rule that does not exist", async () => {
+        expect(await utils.usbRedirection.deleteRule(3333,6666)).to.be.false
+    })
+
+    it("should return true if the rule has been deleted and is not in the list anymore", async () => {
+        var del = null
+        var found = null
+        del = await utils.usbRedirection.deleteRule(1234, 1234)
+        await utils.sleep(500)
+        found = await utils.usbRedirection.findRule(1234, 1234)
+        await utils.sleep(500)
+        console.log("Del: "+del+", found: "+found)
+        expect(del && found == false).to.be.true
+    })
+
+    it("should return true if the rule has been deleted and is not in the database anymore", async () => {
+        var del = null
+        var found = null
+        del = await utils.usbRedirection.deleteRule(1234, 1234)
+        await utils.sleep(500)
+        found = await db.getUSBFromVidPid(1234, 1234)
+        await utils.sleep(500)
+        expect(del && found == null).to.be.true
+    })
+})
