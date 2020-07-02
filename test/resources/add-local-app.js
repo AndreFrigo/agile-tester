3612b0a91781ce37dad44e9325a089832bb56f89
Andrea Frigo <andrea.boy.frigo@gmail.com>
2020-07-02 15:03:06 +0200

    add resources/addd-local-app.js, incomplete

---
+++ test/resources/add-local-app.js
@@ -0,0 +1,36 @@
+const {db} = require ("../db.js");
+const {global} = require ("../global.js");
+const {utils} = require("../utils.js");
+const { expect } = require("chai");
+var localDB = null
+
+describe("test", function(){
+
+    this.timeout(30000)
+
+    before(async function(){
+        //salva database locale
+        db.conn.select(1)
+        localDB = await new Promise(function (resolve, reject){
+            db.conn.get("connections", function(err, res){
+                if(err) reject(err)
+                resolve(res)
+            });
+        })
+    })
+
+    beforeEach(async function(){
+        await global.app.start()
+        //cambia database locale
+        db.conn.set("connections", "[{\"name\":\"test\",\"type\":\"URL\",\"autostart\":false,\"onExitAction\":\"\",\"passthrough\":false,\"local\":true,\"server\":false,\"options\":{\"url\":\"https://prova.it\",\"kiosk\":false,\"fullscreen\":false,\"browser\":\"iexplore\"},\"id\":\"afe39343-6643-49a5-a684-572ead42d3ee\"}]")
+    }) 
+
+    afterEach(async function(){
+        db.conn.set("connections", localDB)
+        //await global.app.stop()
+    })
+
+    it("try", async () => {
+        await utils.resources.addLocal()
+    })
+})
