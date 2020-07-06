73c30b7e49789155bb415640bb6bbe4a2b108a27
Andrea Frigo <andrea.boy.frigo@gmail.com>
2020-07-06 10:17:45 +0200

    add ersources/add-citrix.js

---
+++ test/resources/add-citrix.js
@@ -0,0 +1,52 @@
+const {db} = require ("../db.js");
+const {global} = require ("../global.js");
+const {utils} = require("../utils.js");
+const { expect } = require("chai");
+var localDB = null
+
+describe("test add citrix resource", function(){
+
+    this.timeout(30000)
+
+    before(async function(){
+        //salva database locale
+        db.conn.select(1)
+        localDB = await new Promise(function (resolve, reject){
+            db.conn.get("connections", function(err, res){
+                if(err) reject(err)
+                resolve(res)
+            });
+        })
+    })
+
+    beforeEach(async function(){
+        //cambia database locale
+        db.conn.set("connections", "[{\"name\":\"test\",\"type\":\"CITRIX\",\"autostart\":false,\"onExitAction\":\"\",\"passthrough\":false,\"local\":false,\"server\":true,\"options\":{\"url\":\"https://prova.it\",\"allow_unauthorized\":false,\"hide_topbar\":false,\"force_fullscreen\":false,\"onAgileLogoutAction\":\"\",\"domain\":\"dom\",\"hideDomain\":false,\"exclude\":{\"name\":[],\"type\":[]}},\"id\":\"4eea220f-7e5d-40a5-98d5-87568f20480d\"}]")
+        await utils.start()
+    }) 
+
+    afterEach(async function(){
+        db.conn.set("connections", localDB)
+        await global.app.stop()
+    })
+
+    it("should return true if the resource is in the database", async () => {
+        var found = false
+        const resource = await db.getResourceFromName("test");
+        if(resource && resource.options.url == "https://prova.it"){
+            found = true
+        }
+        expect(found).to.be.true
+    })
+
+
+    it("should return false if it tries to add a resource with the same name", async () => {
+        expect(await utils.resources.addCitrix("test", "https://prova.it", "dom")).to.be.false
+    })
+
+
+    it("should return true if the resource is in the Agile list", async() => {
+        expect(await utils.resources.isInAgileList(1, "test", "https://prova.it")).to.be.true
+    })
+
+})
