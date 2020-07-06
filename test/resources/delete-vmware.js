0f753bba240aeb49782ea2be965b0b7f85f2de69
Andrea Frigo <andrea.boy.frigo@gmail.com>
2020-07-06 14:37:40 +0200

    add resources/delete-vmware.js

---
+++ test/resources/delete-vmware.js
@@ -0,0 +1,70 @@
+const {db} = require ("../db.js");
+const {global} = require ("../global.js");
+const {utils} = require("../utils.js");
+const { expect } = require("chai");
+var localDB = null
+
+describe("delete VMware resource tests", function(){
+
+    this.timeout(30000)
+
+    before(async function(){
+        //salva database locale
+        db.conn.select(1)
+        localDB = await new Promise(function (resolve, reject){
+            db.conn.get("connections", function(err, res){
+                if(err) reject(err)
+                resolve(res)
+            });
+        })
+    })
+
+    beforeEach(async function(){
+        //cambia database locale
+        db.conn.set("connections", "[{\"name\":\"test\",\"type\":\"VMWARE\",\"autostart\":false,\"onExitAction\":\"\",\"passthrough\":false,\"local\":false,\"server\":true,\"options\":{\"url\":\"test.com\",\"allow_unauthorized\":false,\"domain\":\"\",\"hideDomain\":false,\"exclude\":{\"name\":[],\"type\":[]}},\"id\":\"066366b2-804c-4eb5-b0f5-699c8ef66a4d\"}]")
+        await utils.start()
+    }) 
+
+    afterEach(async function(){
+        db.conn.set("connections", localDB)
+        await global.app.stop()
+    })
+
+    it("should return true if a resource is deleted", async () => {
+        expect(await utils.resources.deleteResource("test")).to.be.true
+    })
+
+    it("should return false if it tries to delete a resource that does not exist", async () => {
+        expect(await utils.resources.deleteResource("wrong_name")).to.be.false
+    })
+
+    it("should return true if a resource is deleted and is not in the database anymore", async () => {
+        var del = null
+        var check = null
+        del = await utils.resources.deleteResource("test")
+        await utils.sleep(1000)
+        check = await db.getResourceFromName("test")
+        expect(del && check == null).to.be.true
+    })
+
+    it("should return true if a resource is deleted and is not in the list anymore", async () => {
+        var del = null
+        var found = false;
+        del = await utils.resources.deleteResource("test")
+        await utils.sleep(500)
+        found = await utils.resources.isInAgileList(3, "test", "test.com")
+        expect(del && found == false).to.be.true
+    })
+
+    it("should return true if a resource is deleted and the success notification appeared", async () => {
+        var del = null
+        var notification = null;
+
+        del = await utils.resources.deleteResource("test")
+        
+        await utils.sleep(1000)
+
+        notification = await utils.checkSuccessNotification()
+        expect(del && notification).to.be.true
+    })
+})
