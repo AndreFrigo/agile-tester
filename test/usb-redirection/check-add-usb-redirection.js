915d4caadf93c9b5e90898386c3d3d6d80f9b87f
AndreFrigo <andrea.boy.frigo@gmail.com>
2020-07-10 09:35:23 +0200

    restore usb-redirection/* because the functions here don't work

--- test/usb-redirection/check-add-usb-redirection.js
+++ test/usb-redirection/check-add-usb-redirection.js
@@ -2,21 +2,21 @@ const {db} = require ("../db.js");
 const {global} = require ("../global.js");
 const {utils} = require("../utils.js");
 const { expect } = require("chai");
-const agileService = require("agile-os-interface")
-
 var localDB = null
 
 describe("Check add usb redirection rule", function(){
 
     this.timeout(30000)
 
+    //qui le funzioni di agile-os-interface non funzionano correttamente
     before(async function(){
         //salva database locale
-        localDB = await new Promise(function(resolve, reject){
-            agileService.getCitrixConfig(null, (err,res) => {
-                if (err) reject(err)
+        db.conn.select(1)
+        localDB = await new Promise(function (resolve, reject){
+            db.conn.get("config_citrix", function(err, res){
+                if(err) reject(err)
                 resolve(res)
-            })
+            });
         })
     })
 
@@ -24,13 +24,6 @@ describe("Check add usb redirection rule", function(){
         //cambia database locale
         db.conn.select(1)
         db.conn.set("config_citrix", "{\"usb_redirection\":{\"auto_redirect_on_start\":true,\"auto_redirect_on_plug\":false,\"rules\":[{\"type\":\"ALLOW\",\"vid\":\"1234\",\"pid\":\"1234\",\"class\":\"\",\"subclass\":\"\",\"prot\":\"\",\"description\":\"prova_usb\",\"split\":false,\"interfaces\":[]}],\"default\":\"ALLOW\"}}")
-        
-        console.log(await new Promise(function(resolve, reject){
-            agileService.getCitrixConfig(null, (err,res) => {
-                if (err) reject(err)
-                resolve(res)
-            })
-        }))
         await utils.start()
     }) 
 
