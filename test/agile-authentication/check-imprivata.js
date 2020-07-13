840c2b67279cc953175b6462131d767b58bf9bd0
Andrea Frigo <andrea.boy.frigo@gmail.com>
2020-07-13 17:33:52 +0200

    Add test to agile-authentication/check-imprivata.js

--- test/agile-authentication/check-imprivata.js
+++ test/agile-authentication/check-imprivata.js
@@ -59,4 +59,28 @@ describe("Check imprivata authentication settings", function(){
         })
     })
 
+    const realValues = [
+        "https://onesign.dev.praim.com",
+        "https://onesign6.dev.praim.com"
+    ]
+    realValues.forEach(elem => {
+        it("should return true if authentication has been added and test is success", async () => {
+            const add = await utils.agileAuthentication.addImprivata(elem)
+
+            await utils.sleep(500)
+
+            //preme su test
+            try{
+                await global.app.client.$("#main-div > div.main-content > main > section > div > ul > li > div.collapsible-body > div:nth-child(4) > div:nth-child(1) > a").click()
+            }catch{
+            }
+
+            await utils.sleep(1500)
+            var not = null
+            not = await utils.checkNotification("success", 15)
+
+            expect(add && not).to.be.true
+        })
+    })
+
 })
